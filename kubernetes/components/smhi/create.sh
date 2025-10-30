35b4b66b31ff173e0a603a841bac08995ec47caf
Joakim Berglund <joakim.b.berglund@gmail.com>
2025-10-30 19:23:39 +0100

    cleanup

--- kubernetes/components/smhi/create.sh
+++ kubernetes/components/smhi/create.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
-docker build -t smhi:latest .
+docker build -t registry.cube.local:5000/smhi:2.2 .
 #docker tag smhi:latest registry.cube.local:5000/smhi
-#docker push registry.cube.local:5000/smhi
+docker push registry.cube.local:5000/smhi:2.2
 
-kubectl apply -f smhi.yaml
+#kubectl apply -f smhi.yaml
 
