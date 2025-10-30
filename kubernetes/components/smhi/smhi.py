f664c8fa321b96d2a02d35b6fe2e23f6e50ed356
Joakim Berglund <joakim.b.berglund@gmail.com>
2025-10-30 12:53:05 +0100

    Added input parameters lat long --test

--- kubernetes/components/smhi/smhi.py
+++ kubernetes/components/smhi/smhi.py
@@ -3,15 +3,38 @@
 import time
 from calendar import timegm
 import requests
+import sys
 
-coord = "62.406521, 17.362494"
+#coord = "62.406521, 17.362494"
 
+if len(sys.argv) < 3:
+  print("Usage: python coords.py <lat> <long> [--test [value]]", file=sys.stderr)
+    sys.exit(1)
+    
+  # Parse latitude and longitude (first two args)
+  try:
+    lat = float(sys.argv[1])
+    long = float(sys.argv[2])
+  except ValueError:
+    print("Error: lat and long must be valid numbers.", file=sys.stderr)
+    sys.exit(1)
+
+if len(sys.argv) > 3:
+  if sys.argv[3] == '--test':
+    test_is_set = True
+    # If there's a value after --test
+    if len(sys.argv) > 4:
+      test_value = sys.argv[4]
+    else:
+      test_value = 'true'  # default when --test is used without value
+ 
 def write_influxdb( out ):
-  r = requests.post("http://influxdb:8086/write?db=smhi", data=out)
-#  print(out)
+  if test_is_set:
+    print(out)
+  else
+    r = requests.post("http://influxdb:8086/write?db=smhi", data=out)
 
-r = requests.get('http://opendata-download-metfcst.smhi.se/api/category/pmp3g/version/2/geotype/point/lon/17.362494/lat/62.406521/data.json')
-
+r = requests.get('http://opendata-download-metfcst.smhi.se/api/category/pmp3g/version/2/geotype/point/lon/f"{long}/{lat}"17.362494/lat/62.406521/data.json')
 data = r.json()
 
 for nr in  data["timeSeries"]:
