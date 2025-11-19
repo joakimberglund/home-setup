312d040c07c455a982598e2cd905c11105b0a1c7
Joakim Berglund <joakim.b.berglund@gmail.com>
2025-11-19 19:29:50 +0100

    changed to bash

--- kubernetes/install-helm.sh
+++ kubernetes/install-helm.sh
@@ -4,5 +4,5 @@ cd
 mkdir -p ~/.kube
 k3s kubectl config view --raw > /root/.kube/config
 chmod 600 /root/.kube/config
-curl -fsSL https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-3 | sh -s -
+curl -fsSL https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-3 | bash -s -
 helm version
